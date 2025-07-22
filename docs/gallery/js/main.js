9739540fbc744ee82d462dcdaeaf95ccb4a181aa
luis <luis@luiss-MacBook-Pro.local>
2025-07-22 20:31:59 +0200

    Removing old files

--- docs/gallery/js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var grid = document.querySelector('.grid');
-  imagesLoaded(grid, function() {
-    new Masonry(grid, { itemSelector: '.grid-item', gutter: 10, percentPosition: true });
-  });
-});
